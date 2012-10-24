89a07e83ea41260d04c4a87972a0d5917fb361fd
Brent Bradbury <bbradbury@peak6.com>
2012-10-24 18:28:24 -0500

    Fix pid paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,4 +7,5 @@ timeout 30         # restarts workers that hang for 30 seconds
 # so prevent them from going to /dev/null when daemonized here:
 stderr_path "#{File.dirname __FILE__}/../log/unicorn.stderr.log"
 stdout_path "#{File.dirname __FILE__}/../log/unicorn.stdout.log"
+pid "#{File.dirname __FILE__}/../tmp/pids/unicorn.pid"
 listen 4000
